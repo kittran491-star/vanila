c9f1268a1cd846ed3b918ed10090bf0d90c74c84
kittran491-star <kittran491@gmail.com>
2026-09-15 16:45:43 +0700

    Remove hardcoded cover image in game.js
    Remove hardcoded cover image source in button click handler.

--- game.js
+++ game.js
@@ -24,7 +24,6 @@ document.querySelectorAll('.card-choice').forEach(btn=>{
   chosen = +btn.dataset.id;
 
 // Luôn hiện ảnh bìa, không lộ kết quả
-$('previewImage').src = "assets/cover.jpg";
 
 $('flipCard').classList.remove('flipped');
 show('flip');
